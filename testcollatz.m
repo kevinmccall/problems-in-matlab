2bfa144a2663efc22f2fad7f994f363ea163b60c
kevinmccall <erakiproj99@gmail.com>
2022-12-11 11:52:40 -0500

    made plot better for collatz

--- testcollatz.m
+++ testcollatz.m
@@ -1,6 +1,10 @@
-graphics_toolkit("qt")
 x = 1:1000
 for i=1:1000
 	y(i)=collatz(i)
 endfor
-scatter(x,y,'.')
+scatter(x,y, 5, 'b', "filled")
+title('Collatz Conjecture Visualization for N=1000');
+xlabel('Starting Number');
+ylabel('Iterations until value 1');
+set(gca, 'FontSize', 14);
+
